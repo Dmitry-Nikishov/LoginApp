83bea78e50f6a4df85ef69abbdeb9be4d3c5b0d0
Dmitry Nikishov <dmitry86nik@gmail.com>
2022-10-03 00:05:06 +0300

    gradient impl

--- LoginApp/ViewControllers/WelcomeViewController.swift
+++ LoginApp/ViewControllers/WelcomeViewController.swift
@@ -13,8 +13,24 @@ final class WelcomeViewController: UIViewController {
     
     var userName: String = ""
     
+    lazy var gradient: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        gradient.type = .axial
+        gradient.colors = [
+            UIColor.cyan.cgColor,
+            UIColor.blue.cgColor,
+            UIColor.cyan.cgColor
+        ]
+        gradient.locations = [0, 0.4, 0.9]
+        return gradient
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gradient.frame = view.bounds
+
+        view.layer.insertSublayer(gradient, at: 0)
         
         welcomeLabel.text = "Welcome, \(userName)"
     }
